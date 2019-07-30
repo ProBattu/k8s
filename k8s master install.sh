fceb68a4071d93430a935d3c61060537a7a47009
rony <rony@example.com>
2019-07-30 18:14:45 +0530

    master node install pod exapmle

---
+++ k8s master install.sh
@@ -0,0 +1,21 @@
+sudo -i
+apt-get update
+apt-get install docker.io
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+# kubeadm init
+# kubeadm init --apiserver-advertise-address=<ip-address-of-kmaster-vm> --pod-network-cidr=192.168.0.0/16
+
+# Copy the kubeadm init output and Become a normal user and enter follow commands
+
+# mkdir -p $HOME/.kube
+# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# kubectl get pods --all-namespaces
---
+++ k8s node install.sh
@@ -0,0 +1,16 @@
+apt-get update
+apt-get install docker.io -y
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+# Below link we can get it from master after kubeadm init command
+# kubeadm join 172.31.85.104:6443 --token 3tnjyo.wr9npd5d4ad49khe \
+#   --discovery-token-ca-cert-hash sha256:7219f073dfbe57e278f47dc460d9870bbb2643b53b2defd2fff7eb6d912a1be2
+
+# Go to k8s Master type below command you will get one node is added
+# Kubectl get nodes
